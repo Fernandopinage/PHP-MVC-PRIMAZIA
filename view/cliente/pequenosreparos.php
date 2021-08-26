ee1fbe7733326fcf67acca10907093f16c4f91df
FernandoPinage <luizfernandoluck@hotmail.com>
2021-08-26 11:21:17 -0400

    atualizando promazia

--- view/cliente/pequenosreparos.php
+++ view/cliente/pequenosreparos.php
@@ -43,6 +43,7 @@ if (isset($_POST['pequenosreparos'])) {
 ?>
 <link href="../../layout/css/cliente_pequenosreparos.css" rel="stylesheet">
 <div class="container-fluid">
+<a href="../../view/cliente/painel.php" class="btn btn-success" style="position: relative; top:50px;"><img src="../../images/left-arrow.png" width="28px" alt=""></a>
     <div class="container" id="registro">
         <div class="text-center">
             <img id="logo" src="../../images/primazia.png" class="img"><br>
