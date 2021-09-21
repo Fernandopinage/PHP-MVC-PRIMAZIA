424ef4189f5cde2cd0e1ee88fe99276f7d2e1631
FernandoPinage <luizfernandoluck@hotmail.com>
2021-09-21 16:52:07 -0400

    ok sql

--- sql/bkp.sql
+++ sql/bkp.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 10-Set-2021 às 15:27
+-- Tempo de geração: 21-Set-2021 às 22:51
 -- Versão do servidor: 10.4.18-MariaDB
 -- versão do PHP: 8.0.3
 
@@ -20,6 +20,28 @@ SET time_zone = "+00:00";
 --
 -- Banco de dados: `primazia`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `admin`
+--
+
+CREATE TABLE `admin` (
+  `admin_id` int(11) NOT NULL,
+  `admin_nome` varchar(100) NOT NULL,
+  `admin_senha` varchar(100) NOT NULL,
+  `admin_email` varchar(100) NOT NULL,
+  `admin_foto` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Extraindo dados da tabela `admin`
+--
+
+INSERT INTO `admin` (`admin_id`, `admin_nome`, `admin_senha`, `admin_email`, `admin_foto`) VALUES
+(1, 'nome', '63a9f0ea7bb98050796b649e85481845', 'luiz.c@progride.com.br', 'pp.jpg'),
+(2, 'nome', 'root', 'email@email.com.br', '');
 
 -- --------------------------------------------------------
 
@@ -51,7 +73,10 @@ CREATE TABLE `cliente` (
 --
 
 INSERT INTO `cliente` (`CLIENTE_ID`, `CLIENTE_NOME`, `CLIENTE_CPF`, `CLIENTE_EMAIL`, `CLIENTE_TELEFONE`, `CLIENTE_CEP`, `CLIENTE_FOTO`, `CLIENTE_SENHA`, `CLIENTE_UF`, `CLIENTE_CIDADE`, `CLIENTE_LOGRADOURO`, `CLIENTE_BAIRRO`, `CLIENTE_COMPLEMENTO`, `CLIENTE_OPCAO`, `CLIENTE_RAZAO`, `CLIENTE_NUM`) VALUES
-(14, 'Rhuan', '053.893.142-63', 'rhuan.v@progride.com.br', '(92) 99999-9999', 69, '', '202cb962ac59075b964b07152d234b70', 'AM', 'Manaus', 'Rua Hannibal Porto', 'Santa Luzia', 'casa 01', 'F', '', '285');
+(14, 'Rhuan', '053.893.142-63', 'rhuan.v@progride.com.br', '(92) 99999-9999', 69, '', '202cb962ac59075b964b07152d234b70', 'AM', 'Manaus', 'Rua Hannibal Porto', 'Santa Luzia', 'casa 01', 'F', '', '285'),
+(15, 'luiz fernando pinage', '004.963.342-20', 'luiz.c@progride.com.br', '(92) 99999-9999', 69093, '', '202cb962ac59075b964b07152d234b70', 'AM', 'Manaus', 'Rua Pio IX', 'Monte das Oliveiras', 'proximo ao manoa', 'F', '', '034'),
+(16, 'aaa', '0045999559595', 'luizfernandoluck@hotmail.com', '(92) 99999-9999', 69006262, '', '63a9f0ea7bb98050796b649e85481845', 'AM', 'Manaus', 'Rua 11 de Agosto', 'Gilberto Mestrinho', 'complemt', 'F', '', '034'),
+(17, 'luiz', '011155245225', 'luiz@homail.com', '(92) 99999-9999', 69006262, '', '202cb962ac59075b964b07152d234b70', 'AM', 'Manaus', 'Rua 11 de Agosto', 'Gilberto Mestrinho', 'complemento', 'F', '', '');
 
 -- --------------------------------------------------------
 
@@ -84,11 +109,55 @@ CREATE TABLE `pedido` (
 INSERT INTO `pedido` (`pedido_id`, `pedido_nome`, `pedido_telefone`, `pedido_email`, `pedido_cpf`, `pedido_cep`, `pedido_data`, `pedido_descricao`, `pedido_uf`, `pedido_cidade`, `pedido_logradouro`, `pedido_bairro`, `pedido_complemento`, `pedido_protocolo`, `pedido_numero`) VALUES
 (19, 'Rhuan', '(92) 99999-999', 'rhuan.v@progride.com.br', '053.893.142-63', '69', '2021-09-10 03:00:32', '{\"tpservico\":\"Artífice (Pedreiro,Pintor e Hidráulico)\",\"categoria\":[\"hidraulica\",\"eletrica\"],\"descricao\":\"Me faça uma breve descrição do serviço a ser realizado\"}', 'AM', 'Manaus', '', 'Santa Luzia', 'casa 01', '2361 030032100921', '285'),
 (20, 'Rhuan', '(92) 99999-999', 'rhuan.v@progride.com.br', '053.893.142-63', '69', '2021-09-10 03:08:03', '{\"tpservico\":\"Artífice (Pedreiro,Pintor e Hidráulico)\",\"categoria\":[\"hidraulica\",\"eletrica\",\"gesso\"],\"descricao\":\"Me faça uma breve descrição do serviço a ser realizado\"}', 'AM', 'Manaus', 'Rua Hannibal Porto', 'Santa Luzia', 'casa 01', '7685 030803100921', '285'),
-(21, 'Rhuan', '(92) 99999-999', 'rhuan.v@progride.com.br', '053.893.142-63', '69', '2021-09-10 03:13:14', '{\"tpservico\":\"Artífice (Pedreiro,Pintor e Hidráulico)\",\"categoria\":[\"hidraulica\",\"eletrica\"],\"descricao\":\"Me faça uma breve descrição do serviço a ser realizado\"}', 'AM', 'Manaus', 'Rua Hannibal Porto', 'Santa Luzia', 'casa 01', '9616 031314100921', '285');
+(21, 'Rhuan', '(92) 99999-999', 'rhuan.v@progride.com.br', '053.893.142-63', '69', '2021-09-10 03:13:14', '{\"tpservico\":\"Artífice (Pedreiro,Pintor e Hidráulico)\",\"categoria\":[\"hidraulica\",\"eletrica\"],\"descricao\":\"Me faça uma breve descrição do serviço a ser realizado\"}', 'AM', 'Manaus', 'Rua Hannibal Porto', 'Santa Luzia', 'casa 01', '9616 031314100921', '285'),
+(22, 'luiz fernando pinage', '(92) 99999-999', 'luiz.c@progride.com.br', '004.963.342-20', '69093', '2021-09-10 04:15:10', '{\"tpservico\":\"Motoboy\",\"categoria\":[\"Enviar um documento\",\"Enviar um pacote\",\"\"]}', 'AM', 'Manaus', 'Rua Pio IX', 'Monte das Oliveiras', 'proximo ao manoa', '5513 041510100921', '034'),
+(23, 'luiz fernando pinage', '(92) 99999-999', 'luiz.c@progride.com.br', '004.963.342-20', '69093', '2021-09-21 04:26:43', '{\"tpservico\":\"Diarista\",\"categoria\":[\"Limpeza padrão\",\"\"],\"descricao\":[\"Qual a área do imóvel? 101,12m²\"],\"local\":[\"Qual o local do serviço? Apartamento\\/Casa\"],\"dependente\":[\"Animais de estimação\"],\"serviço\":[\"Precisa de serviços adicionais? Passar roupa\",\"\"],\"termo\":\"avulso\"}', 'AM', 'Manaus', 'Rua Pio IX', 'Monte das Oliveiras', 'proximo ao manoa', '1217 042643210921', '034'),
+(24, 'luiz fernando pinage', '(92) 99999-999', 'luiz.c@progride.com.br', '004.963.342-20', '69093', '2021-09-21 04:34:33', '{\"tpservico\":\"Diarista\",\"categoria\":[\"Limpeza pesada\",\"\"],\"descricao\":[\"Qual a área do imóvel? 106,04m²\"],\"local\":[\"Qual o local do serviço? Comercial\"],\"dependente\":[\"Animais de estimação\"],\"serviço\":[\"Precisa de serviços adicionais? Lavar roupa\",\"\"],\"termo\":\"mensal\"}', 'AM', 'Manaus', 'Rua Pio IX', 'Monte das Oliveiras', 'proximo ao manoa', '6738 043433210921', '034'),
+(25, 'luiz fernando pinage', '(92) 99999-999', 'luiz.c@progride.com.br', '004.963.342-20', '69093', '2021-09-21 04:36:44', '{\"tpservico\":\"Diarista\",\"categoria\":[\"Limpeza padrão\",\"\"],\"descricao\":[\"Qual a área do imóvel? 106,04m²\"],\"local\":[\"Qual o local do serviço? Apartamento\\/Casa\"],\"dependente\":[\"Selecione\"],\"serviço\":[\"Precisa de serviços adicionais? Outros\",\"teste de texto outros\"],\"termo\":\"mensal\"}', 'AM', 'Manaus', 'Rua Pio IX', 'Monte das Oliveiras', 'proximo ao manoa', '6347 043644210921', '034');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `profissional`
+--
+
+CREATE TABLE `profissional` (
+  `profissional_id` int(11) NOT NULL,
+  `profissional_nome` varchar(100) NOT NULL,
+  `profissional_option` varchar(1) NOT NULL,
+  `profissional_razao` varchar(100) NOT NULL,
+  `profissional_email` varchar(100) NOT NULL,
+  `profissional_cpf` varchar(14) NOT NULL,
+  `profissional_telefone` varchar(15) NOT NULL,
+  `profissional_cep` varchar(9) NOT NULL,
+  `profissional_uf` varchar(2) NOT NULL,
+  `profissional_logradouro` varchar(100) NOT NULL,
+  `profissional_num` varchar(6) NOT NULL,
+  `profissional_cidade` varchar(100) NOT NULL,
+  `profissional_bairro` varchar(100) NOT NULL,
+  `profissional_complemento` varchar(100) NOT NULL,
+  `profissional_foto` longtext NOT NULL,
+  `profissional_senha` varchar(100) NOT NULL,
+  `profissional_servico` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Extraindo dados da tabela `profissional`
+--
+
+INSERT INTO `profissional` (`profissional_id`, `profissional_nome`, `profissional_option`, `profissional_razao`, `profissional_email`, `profissional_cpf`, `profissional_telefone`, `profissional_cep`, `profissional_uf`, `profissional_logradouro`, `profissional_num`, `profissional_cidade`, `profissional_bairro`, `profissional_complemento`, `profissional_foto`, `profissional_senha`, `profissional_servico`) VALUES
+(1, 'luiz', 'F', '', 'luiz.c@progride.com.br', '004.963.342-20', '(92) 99999-9999', '69006262', 'AM', 'Rua 11 de Agosto', '034', 'Manaus', 'Gilberto Mestrinho', '', '', '63a9f0ea7bb98050796b649e85481845', 'Artífice (Pedreiro,Pintor e Hidráulico)');
 
 --
 -- Índices para tabelas despejadas
 --
+
+--
+-- Índices para tabela `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`admin_id`),
+  ADD UNIQUE KEY `admin_email` (`admin_email`);
 
 --
 -- Índices para tabela `cliente`
@@ -105,20 +174,40 @@ ALTER TABLE `pedido`
   ADD PRIMARY KEY (`pedido_id`);
 
 --
+-- Índices para tabela `profissional`
+--
+ALTER TABLE `profissional`
+  ADD PRIMARY KEY (`profissional_id`),
+  ADD UNIQUE KEY `profissional_email` (`profissional_email`),
+  ADD UNIQUE KEY `profissional_cpf` (`profissional_cpf`);
+
+--
 -- AUTO_INCREMENT de tabelas despejadas
 --
+
+--
+-- AUTO_INCREMENT de tabela `admin`
+--
+ALTER TABLE `admin`
+  MODIFY `admin_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT de tabela `cliente`
 --
 ALTER TABLE `cliente`
-  MODIFY `CLIENTE_ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+  MODIFY `CLIENTE_ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
 
 --
 -- AUTO_INCREMENT de tabela `pedido`
 --
 ALTER TABLE `pedido`
-  MODIFY `pedido_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
+  MODIFY `pedido_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
+
+--
+-- AUTO_INCREMENT de tabela `profissional`
+--
+ALTER TABLE `profissional`
+  MODIFY `profissional_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
