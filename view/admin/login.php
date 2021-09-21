285d2394ad1a0c0c43a986c5ae00571c8db91332
FernandoPinage <luizfernandoluck@hotmail.com>
2021-09-21 14:13:16 -0400

    atualizando Primazia

---
+++ view/admin/login.php
@@ -0,0 +1,59 @@
+<?php
+
+include_once "../../layout/heard.php";
+include_once  "../../class/ClassCliente.php";
+include_once "../../dao/ClienteDAO.php";
+
+    if(isset($_POST['loginenviar'])){
+
+        $ClienteClass = new Cliente();
+        $ClienteClass->SetEmail($_POST['email']);
+        $ClienteClass->SetSenha($_POST['senha']);
+
+        $Cliemte = new ClienteDAO();
+        $Cliemte->validarLogin($ClienteClass);
+    }   
+
+?>
+
+<link href="../../layout/css/profissional_login.css" rel="stylesheet">
+<div class="container-fluid">
+    <div class="text-center">
+        <img id="logo" src="../../images/primazia.png" class="img"><br>
+    </div>
+
+    <div class="title text-center">
+        <p>LOGIN PROFISSIONAL</p>
+        <h5 id="registro">Já possui um cadastro?</h5>
+       
+    </div>
+    <div class="container">
+        <form class="row g-2" id="form" method="POST">
+
+        <div class="col">
+        </div>
+            <div class="col-12">
+                <label for="exampleInputEmail1" class="form-label">E-mail</label>
+                <input type="email" class="form-control" name="email" id="exampleInputEmail1" aria-describedby="emailHelp">
+            </div>
+            <div class="col-12">
+                <label for="exampleInputPassword1" class="form-label">Senha</label>
+                <input type="password" class="form-control" name="senha" id="exampleInputPassword1">
+            </div>
+            <div>
+                <a id="registro" href="../cliente/registro.php">Criar uma conta</a>
+                
+            </div>
+            <div class="d-grid text-center">
+                <button type="submit" name="loginenviar" class="btn btn-lg orangered">ENVIAR</button>
+            </div>
+        </form>
+    </div>
+
+</div>
+
+
+<?php
+
+include_once "../../layout/footer.php";
+?>
---
+++ view/admin/painel.php
@@ -0,0 +1,80 @@
+<?php
+include_once "../../layout/heard.php";
+
+//session_start();
+
+//if (empty($_SESSION['user'])) {
+
+    //header('Refresh: 0.1; url=login.php');
+//}
+
+?>
+<link href="../../layout/css/admin_painel.css" rel="stylesheet">
+<div id="logo">
+    <img src="../../images/primazia.png" alt="" width="250" height="190">
+</div>
+
+
+<div class="container">
+
+    <div class="text-center">
+        <?php
+        if (!empty($_SESSION['user']['foto'])) {
+        ?>
+            <img id="usuario" src="../../images/<?php echo $_SESSION['user']['foto'] ?>" class="img"><br><br>
+        <?php
+
+        } else {
+        ?>
+            <img id="usuario" src="../../images/perfil.png" class="img"><br><br>
+        <?php
+        }
+        ?>
+
+        <h5 style="text-transform: capitalize;"><?php echo $_SESSION['user']['nome'] ?></h5><br>
+        <img src="../../images/photo1629981520.jpeg" class="img" width="130"> 4,67</h5></img><br>
+    </div>
+
+
+</div>
+
+<div class="container-fluid">
+
+    <nav class="nav_bar">
+        <div class="row g-4">
+            <div class="col-md">
+                <a class="navbar-brand" href="">
+                    <img src="../../images/novousuario.png" alt="" width="80" height="80">
+                </a>
+                <p class="fs-6"> Cadastrar Usuário</p>
+            </div>
+            <div class="col-md">
+                <a class="navbar-brand" href="">
+                    <img src="../../images/editarusuario.png" alt="" width="80" height="80">
+                </a>
+                <p class="fs-6"> Editar Usuário </p>
+    </div>
+
+            <div class="col-md">
+                <a class="navbar-brand" href="../cliente/meu_pedidos.php">
+                    <img src="../../images/pedidosquesolicitei.png" alt="" width="70" height="70">
+                </a>
+                <p class="fs-6">Pedidos G2S</p>
+            </div>
+            <div class="col-md">
+                <a class="navbar-brand" href="../../view/cliente/logout.php">
+                    <img src="../../icons/photo1629906564.jpeg" alt="" width="70" height="70">
+                </a>
+                <p class="fs-6">Sair</p>
+            </div>
+        </div>
+    </nav>
+
+</div>
+
+
+
+
+<?php
+require "../../layout/footer.php";
+?>
