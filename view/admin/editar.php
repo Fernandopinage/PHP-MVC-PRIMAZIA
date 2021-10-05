99bf3f420dea816eb9b00d6a9edaf36602e4c62d
FernandoPinage <luizfernandoluck@hotmail.com>
2021-10-05 16:12:25 -0400

    ok

--- view/admin/editar.php
+++ view/admin/editar.php
@@ -520,7 +520,7 @@ if (empty($_SESSION['admin'])) {
                                     </div>
 
                                     <div class="modal-footer">
-                                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Fechar</button>
                                         <input type="submit" name="editar_admin_profissional" class="btn btn-primary" value="Editar">
                                     </div>
 
