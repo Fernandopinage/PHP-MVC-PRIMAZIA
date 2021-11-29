f3455da2c42c62a37b314be124e4e12785f5802a
FernandoPinage <luizfernandoluck@hotmail.com>
2021-11-29 14:49:28 -0400

    ok

--- layout/heard.php
+++ layout/heard.php
@@ -7,7 +7,7 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script> 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
     <link rel="shortcut icon" href="http://primazia.agenciaprogride.com.br/wp-content/uploads/2021/04/LOGO-G2S_SIMBOLOS.png" />
-    <link rel="icon" href="../icons/LOGO-G2S_SIMBOLOS.png" type="image/x-icon" />
+    <link rel="icon" href="../icons/logog2s.png" type="image/x-icon" />
     <title>GoToService – Contrate Serviços Terceirizados</title>
   <!------------------------------- Sweet Alert ---------------------------------->
   <script src="//cdn.jsdelivr.net/npm/sweetalert2@10"></script>
